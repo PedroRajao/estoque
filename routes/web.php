0186a44993601c4b3a82869b2e0df5e3ba82da48
PedroRajao <pedrorajao@gmail.com>
2018-05-28 17:35:25 -0300

    Closed and open routes assigned

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ Route::get('/', function(){
     return "<h1>Listagem</h1>";
 });
 
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/produtos', 'ProdutoController@lista');
 Route::get('/produtos/mostra/{id}', 'ProdutoController@mostra')->where('id', '[0-9]+');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/produtos/novo', 'ProdutoController@novo');
 Route::post('/produtos/adiciona', 'ProdutoController@adiciona');
 Route::get('/produtos/remove/{id}', 'ProdutoController@remove')->where('id', '[0-9]+');
