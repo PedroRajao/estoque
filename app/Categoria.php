c7de2d459e43c90305aeed0d3961b87f509215a9
Pedro Rajão <pedro-marques@prodesan.com>
2018-05-29 18:03:54 -0300

    Add: db:seed da tabela Categoria

--- app/Categoria.php
+++ app/Categoria.php
@@ -4,9 +4,14 @@ namespace estoque;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Categoria extends Model
-{
-    public function produtos(){
+class Categoria extends Model {
+
+    protected $table = 'categorias';
+    public $timestamps = false;
+    protected $fillable = array('nome');
+
+    public function produtos() {
         return $this->hasMany('estoque\Produto');
     }
+
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,30 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use estoque\Categoria;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        Model::unguard();
+        $this->call(CategoriaTableSeeder::class);
     }
+
+}
+
+class CategoriaTableSeeder extends Seeder {
+
+    public function run() {
+        Categoria::create(['nome' => 'ELETRODOMESTICO']);
+        Categoria::create(['nome' => 'ELETRONICA']);
+        Categoria::create(['nome' => 'BRINQUEDO']);
+        Categoria::create(['nome' => 'ESPORTES']);
+    }
+
 }
